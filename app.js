8af197fb8626bc577c1567a44f51809c1f850fb5
shubham sharma <shubhamtech004@gmail.com>
2025-12-04 17:47:46 +0530

    server setupp done

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-import 'dotenv/config';
 import express from "express";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const server = express();
 
-dotenv.config({ path: './.env' });
 server.use(
     cors({
-        origin:[process.env.FRONTEND_URL],
-        margin: ["GET", "POST" , "PUT", "DELETE"],
-        credentials:true
-})
+        origin: [process.env.FRONTEND_URL],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
 );
+
 server.use(cookieParser());
 server.use(express.json());
-server.use(express.urlencoded({extended:true}));
+server.use(express.urlencoded({ extended: true }));
 
-export {server}
+export { server };
