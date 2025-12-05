d768d2d1a4bb3eede8e203250bc9e8be902dcd61
shubham sharma <shubhamtech004@gmail.com>
2025-12-05 23:53:46 +0530

    db connection
    database connection is done using the mongodb cloud atlas

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 // uri in .env
 const uri = process.env.URI;
 
-const ConnectDb = () =>{
-    mongoose.connect(uri,{
-        dbName:"MERN_AUTH"
-    }).then(()=>{
-        console.log("Db connection sucessfully");
-    }).catch((err)=>{
-        console.log("getting error while connecteed to db", err);
-        
+const ConnectDb = () => {
+  mongoose
+    .connect(uri, {
+      dbName: "MERN_AUTH",
     })
-}
+    .then(() => {
+      console.log("Db connection sucessfully");
+    })
+    .catch((err) => {
+      console.log("getting error while connecteed to db", err);
+    });
+};
 
-export  {ConnectDb};
+export { ConnectDb };
