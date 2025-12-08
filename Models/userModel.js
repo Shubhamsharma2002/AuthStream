d258eb9a367fb2e88dc2c03c4eaee09da8d05857
Shubham Sharma <shubhamtech004@gmail.com>
2025-12-08 18:55:37 +0530

    user model
    also add the hash password and comparepassword function

---
+++ Models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: {
+      type: String,
+      minLength: [8, "Password must have atleast 8 Character"],
+      maxLength: [32, "Password cannot have more than 32 Character"],
+    },
+    phone: String,
+    accountVerified: { type: Boolean, default: false },
+    verificationCode: Number,
+    verificationCodeExpire: Date,
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+
+// for password encryption
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")){
+        next();
+    }
+    this.password = await bcrypt.hash(this.password,10);
+})
+
+//  for verifying password
+userSchema.methods.comparePassword = async function(enterPassword) {
+     return await bcrypt.compare(enterPassword,this.password)
+}
+
+export const User = mongoose.model("User", userSchema)
